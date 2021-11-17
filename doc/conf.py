5c286da1a7d98f4d9c1f6475eb4bbb69f0b31287
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2021-11-17 16:45:50 +0100

    Add publish to github actions

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Joan Marcè i Igual"
 author = "Joan Marcè i Igual"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.0.0"
+release = "v1.0.2"
 
 
 # -- General configuration ---------------------------------------------------
