73e6ed83a087a6e09b68f51ea27917f5d0fb805e
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2021-11-17 17:14:01 +0100

    Try new deploy

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Joan Marcè i Igual"
 author = "Joan Marcè i Igual"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.0.3"
+release = "v1.0.4"
 
 
 # -- General configuration ---------------------------------------------------
